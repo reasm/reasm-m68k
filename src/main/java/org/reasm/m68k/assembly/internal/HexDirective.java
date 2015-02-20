98e88a007295f30fd68e8e6495a5d0e6b120fbce
Francis Gagné <fragag1@gmail.com>
2015-02-19 19:03:00 -0500

    Replace Character.digit() with a custom function
    Add a function that retrieves the value of an hexadecimal digit. This
    function is more strict than Character.digit(), which supports fancy
    Unicode characters that we don't really care about.

--- src/main/java/org/reasm/m68k/assembly/internal/HexDirective.java
+++ src/main/java/org/reasm/m68k/assembly/internal/HexDirective.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
 
+import org.reasm.commons.source.Syntax;
 import org.reasm.m68k.messages.InvalidCharacterInHexDirectiveErrorMessage;
 import org.reasm.messages.OddNumberOfCharactersInHexDirectiveErrorMessage;
 
@@ -18,6 +19,21 @@ class HexDirective extends Mnemonic {
 
     @Nonnull
     static final HexDirective HEX = new HexDirective();
+
+    private static int getHexDigitValue(int codePoint) {
+        if (Syntax.isDigit(codePoint)) {
+            return codePoint - '0';
+        }
+
+        // 'a'..'f' => 'A'..'F'
+        codePoint &= ~0x20;
+
+        if (codePoint >= 'A' && codePoint <= 'F') {
+            return codePoint - ('A' - 10);
+        }
+
+        return -1;
+    }
 
     private HexDirective() {
     }
@@ -35,15 +51,15 @@ class HexDirective extends Mnemonic {
                 assert currentCodePoint != -1;
 
                 // Parse the first nybble of a nybble pair.
-                int hexCharValue = Character.digit(currentCodePoint, 16);
-                if (hexCharValue == -1) {
+                int hexDigitValue = getHexDigitValue(currentCodePoint);
+                if (hexDigitValue == -1) {
                     context.addMessage(new InvalidCharacterInHexDirectiveErrorMessage(currentCodePoint));
 
                     // Continue to next operand.
                     break;
                 }
 
-                byte byteValue = (byte) (hexCharValue << 4);
+                byte byteValue = (byte) (hexDigitValue << 4);
 
                 reader.advance();
                 reader.skipWhitespace();
@@ -58,15 +74,15 @@ class HexDirective extends Mnemonic {
                 assert currentCodePoint != -1;
 
                 // Parse the second nybble of a nybble pair.
-                hexCharValue = Character.digit(currentCodePoint, 16);
-                if (hexCharValue == -1) {
+                hexDigitValue = getHexDigitValue(currentCodePoint);
+                if (hexDigitValue == -1) {
                     context.addMessage(new InvalidCharacterInHexDirectiveErrorMessage(currentCodePoint));
 
                     // Continue to next operand.
                     break;
                 }
 
-                byteValue |= hexCharValue;
+                byteValue |= hexDigitValue;
 
                 // Add the parsed byte to the buffer.
                 context.builder.appendAssembledData(byteValue);
